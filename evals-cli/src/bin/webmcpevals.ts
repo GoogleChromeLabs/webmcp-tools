55f8d6cf95a3708eff548f87aa44a0d1043c551b
Pascal Birchler <pascalb@google.com>
2026-03-03 15:42:31 +0100

    Clean up old report files on launch

--- evals-cli/src/bin/webmcpevals.ts
+++ evals-cli/src/bin/webmcpevals.ts
@@ -15,7 +15,7 @@ import minimist from "minimist";
 import open from "open";
 import { renderWebmcpReport } from "../report/report.js";
 import { executeInBrowserEvals, listToolsFromPage } from "../evaluator/index.js";
-import { sortObjectKeys } from "../utils.js";
+import { sortObjectKeys, cleanOldReports } from "../utils.js";
 
 dotenv.config();
 
@@ -123,6 +123,7 @@ const report = renderWebmcpReport(config, finalResults);
 
 const reportName = `report-${Date.now()}.html`;
 
+await cleanOldReports();
 await writeFile(reportName, report);
 console.log(`Report saved to ${reportName}`);
 
