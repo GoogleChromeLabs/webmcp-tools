abe2a42a4cf4e796bfbfdb0048ce254ff5b9ee77
Kasper Kulikowski <kaskulikowski@google.com>
2026-05-28 16:18:14 +0000

    Example evals for sport shop

--- evals-cli/src/evaluator/prompts.ts
+++ evals-cli/src/evaluator/prompts.ts
@@ -6,8 +6,8 @@
 export const SYSTEM_PROMPT = `
 # INSTRUCTIONS
 You are an agent helping a user navigate a page via the tools made available to you. You must
-use the provided tools to query page content when you need it.
-CRITICAL RULE: Do not try to use other tools than the available ones.
+use the provided tools to query page content when you absolutely need it.
+CRITICAL RULE: Do not try to use other tools than the available ones. Never use more tool calls than necessary.
 
 # ADDITIONAL CONTEXT
 Today's date is: ${new Date().toDateString()}.
